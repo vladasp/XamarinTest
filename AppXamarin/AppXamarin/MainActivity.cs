e3abf1ea69bba7882060026c933909b5028eba02
vladasp <vladalogin@ukr.net>
2016-02-29 03:24:46 +0200

    New tabs

---
+++ AppXamarin/AppXamarin/MainActivity.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.View;
+using Android.Support.V4.App;
+
+namespace AppXamarin
+{
+    [Activity(Label = "AppXamarin", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FragmentActivity
+    {
+        private ViewPager vp;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            vp = FindViewById<ViewPager>(Resource.Id.viewPager);
+            vp.Adapter = new AwesomeFragmentAdapter(SupportFragmentManager);
+        }
+    }
+
+    public class AwesomeFragmentAdapter : FragmentPagerAdapter
+    {
+        List<string> items = new List<string>();
+
+        public AwesomeFragmentAdapter(Android.Support.V4.App.FragmentManager fm): base(fm)
+        {
+            items.Add("1 page");
+            items.Add("2 page");
+            items.Add("3 page");
+            items.Add("4 page");
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+            Android.Support.V4.App.Fragment fr = new Android.Support.V4.App.Fragment();
+            switch (position)
+            {
+                case 0:
+                    fr = new FirstFragment();
+                    break;
+                case 1:
+                    fr = new SecondFragment();
+                    break;
+                case 2:
+                    fr = new ThirdFragment();
+                    break;
+                case 3:
+                    fr = new FourthFragment();
+                    break;
+            }
+            return fr;
+        }
+    }
+
+    public class FirstFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.Contacts, container, false);
+            return view;
+        }
+    }
+
+    public class SecondFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.Collection, container, false);
+            return view;
+        }
+    }
+
+    public class ThirdFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.DB, container, false);
+            return view;
+        }
+    }
+
+    public class FourthFragment : Android.Support.V4.App.Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.Image, container, false);
+            return view;
+        }
+    }
+
+
+}
+
--- AppXamarin/AppXamarin/Resources/Resource.Designer.cs
+++ AppXamarin/AppXamarin/Resources/Resource.Designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("XamarinTest.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("AppXamarin.Resource", IsApplication=true)]
 
-namespace XamarinTest
+namespace AppXamarin
 {
 	
 	
@@ -57,14 +57,51 @@ namespace XamarinTest
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView2 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView3 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView4 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int viewPager = 2131034116;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int ContactsView = 2130903040;
+			public const int Collection = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int Contacts = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int DB = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Image = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int Main = 2130903044;
 			
 			static Layout()
 			{
